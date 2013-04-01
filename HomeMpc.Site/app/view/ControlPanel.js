f1e704a036d37580495a1e652fc8ccba686bc323
Denis Dmitriev <denis.dmitriev@gmail.com>
2013-04-01 19:35:20 +0300

    Add logo to the top-left corner

--- HomeMpc.Site/app/view/ControlPanel.js
+++ HomeMpc.Site/app/view/ControlPanel.js
@@ -1,34 +1,45 @@
 ﻿Ext.define('HomeMPC.view.ControlPanel', {
   extend: 'Ext.panel.Panel',
   alias: 'widget.controlpanel',
-  tbar: [{
-    xtype: 'buttongroup',
-    items: [{
-      text: 'Play',
-      action: 'play',
-      scale: 'large',
-      icon: '/resources/images/play-blue.png'
+  layout: 'hbox',
+  bodyCls: 'x-btn-group-default-framed-mc',
+  items: [{
+    xtype: 'panel',
+    width: 52,
+    height: 48,
+    html: '<img src="/resources/images/logo.png" width="48" height="48" border="0" alt="MPC Home" title="Home MPC" />',
+    border: 0,
+    bodyPadding: "0 2 0 2",
+    bodyCls: 'x-btn-group-default-framed-mc',
     },{
-      text: 'Pause',
-      action: 'pause',
-      scale: 'large',
-      icon: '/resources/images/pause-blue.png'
-    },{
-      text: 'Stop',
-      action: 'stop',
-      scale: 'large',
-      icon: '/resources/images/stop-blue.png'
-    },{
-      text: 'Prev',
-      action: 'prev',
-      scale: 'large',
-      icon: '/resources/images/prev-blue.png'
-    },{
-      text: 'Next',
-      action: 'next',
-      scale: 'large',
-      icon: '/resources/images/next-blue.png'
-    }]
+      xtype: 'buttongroup',
+      bodyPadding: 2,
+      items: [{
+        text: 'Play',
+        action: 'play',
+        scale: 'large',
+        icon: '/resources/images/play-blue.png'
+      },{
+        text: 'Pause',
+        action: 'pause',
+        scale: 'large',
+        icon: '/resources/images/pause-blue.png'
+      },{
+        text: 'Stop',
+        action: 'stop',
+        scale: 'large',
+        icon: '/resources/images/stop-blue.png'
+      },{
+        text: 'Prev',
+        action: 'prev',
+        scale: 'large',
+        icon: '/resources/images/prev-blue.png'
+      },{
+        text: 'Next',
+        action: 'next',
+        scale: 'large',
+        icon: '/resources/images/next-blue.png'
+      }]
   }],
   
   init: function () {
